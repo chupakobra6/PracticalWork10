74296b67b70936e7f67c0194c52074b6c029a63e
chupakobra6 <isip_i.m.koshenskiy@mpt.ru>
2022-12-22 04:28:08 +0300

    im crying for joy

---
+++ ICrud.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp13
+{
+    internal interface ICrud
+    {
+        void Create(); // создание // enum role
+        void Visualization(); // чтение
+        void Update(); // изменение
+        void Delete(); // удаление
+        void Search(); // поиск
+        void More(); // подробнее
+    }
+}
+
